b16e6c0b3e51de05375405fbd66d3643fabc0987
AustinWithoutPowers <austinrudkin@live.com>
2022-12-19 20:47:33 +1300

    Finished day 2 part 1 (also removed gitignore because it is not working and am just going to manually delete the py cache... I can fix it when I am done!

--- Day2/part1_day2.py
+++ Day2/part1_day2.py
@@ -1,4 +1,34 @@
-# INPUT_FILE_PATH = '.\\day2_text.txt'
-INPUT_FILE_PATH = '.\\day2_text_example.txt' # example answer is 15
+import base_code
 
-pass
+INPUT_FILE_PATH = '.\\day2_text.txt'
+# INPUT_FILE_PATH = '.\\day2_text_example.txt' # example answer is 15
+
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
+
+PLAY_DICT = {'A': ROCK, 'B': PAPER, 'C': SCISSORS, 'X': ROCK, 'Y': PAPER, 'Z': SCISSORS}
+
+# takes two integers and calculates the win
+def get_result(opponent_move, player_move):
+  # draw
+  if opponent_move - player_move == 0: return 3 + player_move
+
+  # win
+  if opponent_move - player_move == -1: return 6 + player_move
+  if opponent_move - player_move == 2: return 6 + player_move
+
+  return 0 + player_move
+
+def get_score(input_array):
+  total_points = 0
+  for game in input_array:
+    total_points += get_result(PLAY_DICT[game[0]], PLAY_DICT[game[2]])
+  print(total_points)
+
+# main chunk
+def main():
+  get_score( \
+    base_code.process_input_file(INPUT_FILE_PATH))
+
+main()
