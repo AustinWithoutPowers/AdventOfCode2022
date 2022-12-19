a3b5a6e2508897485d0c7f2d6efeeca2dd52a6ed
AustinWithoutPowers <austinrudkin@live.com>
2022-12-19 21:19:20 +1300

    I feel like I am very close but I just cannot nail it down. Going to sleep on it and hopefully have the solution for tomorrow.

--- Day2/part2_day2.py
+++ Day2/part2_day2.py
@@ -1,4 +1,36 @@
-# INPUT_FILE_PATH = '.\\day2_text.txt'
-INPUT_FILE_PATH = '.\\day2_text_example.txt' # example answer is 15
+import base_code
 
-pass
+INPUT_FILE_PATH = '.\\day2_text.txt'
+# INPUT_FILE_PATH = '.\\day2_text_example.txt' # example answer is 12
+
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
+
+PLAY_DICT = {'A': ROCK, 'B': PAPER, 'C': SCISSORS}
+
+# takes an integer and a letter and calculates the points
+def get_result(opponent_move, outcome):
+  if outcome == 'X':
+    if opponent_move == 1:
+      return 2
+    return 0 + opponent_move - 1
+  elif outcome == 'Y':
+    return 3 + opponent_move
+  else:
+    if opponent_move == 3:
+      return 7
+    return 6 + opponent_move + 1
+
+def get_score(input_array):
+  total_points = 0
+  for game in input_array:
+    total_points += get_result(PLAY_DICT[game[0]], game[2])
+  print(total_points)
+
+# main chunk
+def main():
+  get_score( \
+    base_code.process_input_file(INPUT_FILE_PATH))
+
+main()
