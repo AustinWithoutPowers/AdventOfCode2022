8fdb0a9b8c85326ee3b5a1cd87c118dfd5b063fa
AustinWithoutPowers <austinrudkin@live.com>
2022-12-21 18:38:10 +1300

    Added skeleton code for day 5

---
+++ Day5/part1_day5.py
@@ -0,0 +1,14 @@
+import base_code
+
+INPUT_FILE_PATH = '.\\day5_text.txt'
+# INPUT_FILE_PATH = '.\\day5_text_example.txt' # example answer is X
+
+def custom_function(input_array):
+  pass
+
+# main chunk
+def main():
+  custom_function( \
+    base_code.process_input_file(INPUT_FILE_PATH))
+
+main()
